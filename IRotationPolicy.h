7164759fcc98d55dcf3abac635d8d4a0286c5bfc
liladino <marcellblahota@edu.bme.hu>
2026-04-01 12:02:14 +0200

    added try first fitting strategy

--- IRotationPolicy.h
+++ IRotationPolicy.h
@@ -17,8 +17,52 @@ struct RP_largestFaceUp : IRotationPolicy {
 	/* Order the edges of the bin into a decreasing order.
 	 * This way, the largest face will be x-y (and such the smallest will be height z)
 	 */
-	void rotateBin(Bin3& toPack, const Vec3& limits){
+	void rotateBin(Bin3& toPack, const Vec3& limits) override {
 		std::sort(toPack.extent.begin(), toPack.extent.end(), std::greater<size_t>());
+	}
+};
+
+struct RP_tryFirstFitting : IRotationPolicy {
+	/* Try out the first 5 of the 6 directions, and return the first
+     * which fits within the limits. If none fits, return the 6th 
+     * without check - the caller checks it anyway.
+	 */
+	void rotateBin(Bin3& toPack, const Vec3& limits) override {
+		const static Vec3 rotations[] = 
+			{   
+				{0, 2, 1},
+				{1, 0, 2},
+				{1, 2, 0},
+				{2, 0, 1},
+				{2, 1, 0},
+				{0, 1, 2},
+			};
+		
+		for (size_t i = 0; i < 5; i++) {
+            bool fits = true;
+			for (size_t j = 0; j < 3; j++) {
+				size_t src = rotations[i][j];
+				long long dim = toPack[src];
+
+				if (limits[j] < toPack.getPos(j) + dim) {
+                    fits = false;
+                    break;
+                }
+            }
+            if (fits) {
+                /* all dimensions were good, rotate bin
+                 */
+                auto extent = toPack.extent;
+                toPack[0] = extent[rotations[i][0]];
+                toPack[1] = extent[rotations[i][1]];
+                toPack[2] = extent[rotations[i][2]];
+                return;
+            }
+		}
+        /* If no rotation fits, return the 6th, unchecked orientation.
+         * To save computation, the 6th in the order is the original order,
+         * so no job here.
+         */
 	}
 };
 
@@ -26,8 +70,8 @@ struct RP_minLeftoverSlack : IRotationPolicy {
 	/* Order the edges of the bin to minimize leftover slack, 
 	 * relative to the current container.
 	 */	 
-	void rotateBin(Bin3& toPack, const Vec3& limits){
-		std::vector<Vec3> rotations = 
+	void rotateBin(Bin3& toPack, const Vec3& limits) override {
+		const static Vec3 rotations[] = 
 			{   
 				{0, 1, 2},
 				{0, 2, 1},
